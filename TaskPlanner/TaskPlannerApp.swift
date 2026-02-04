4ce1939999053c0ff7aae25b1516289d3c20d7e5
animerusya <rus.melanin@mail.ru>
2026-02-04 08:52:07 +0200

    Initial Xcode project

---
+++ TaskPlanner/TaskPlannerApp.swift
@@ -0,0 +1,21 @@
+//
+//  TaskPlannerApp.swift
+//  TaskPlanner
+//
+//  Created by Руслан Меланин on 04.02.2026.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct TaskPlannerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
